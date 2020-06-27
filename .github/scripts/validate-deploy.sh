b19e0223f17cef923464bb988ab376a8e02bff32
Sean Sundberg <seansund@us.ibm.com>
2020-06-27 16:15:36 -0500

    Updates validate-deploy logic

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -17,30 +17,13 @@ fi
 
 set -e
 
-if [[ "${CLUSTER_TYPE}" == "kubernetes" ]] || [[ "${CLUSTER_TYPE}" == "iks" ]]; then
-  ENDPOINTS=$(kubectl get ingress -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{range .spec.rules[*]}{"https://"}{.host}{"\n"}{end}{end}')
-else
-  ENDPOINTS=$(kubectl get route -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{"https://"}{.spec.host}{"\n"}{end}')
+if [[ $(kubectl get pipelines -n "${NAMESPACE}" | wc -l) -eq 0 ]]; then
+  echo "No pipelines found"
+  exit 1
 fi
-
-echo "Validating endpoints:"
-echo "${ENDPOINTS}"
-
-echo "${ENDPOINTS}" | while read endpoint; do
-  if [[ -n "${endpoint}" ]]; then
-    ${SCRIPT_DIR}/waitForEndpoint.sh "${endpoint}" 10 10
-  fi
-done
-
-CONFIG_URLS=$(kubectl get configmap -n "${NAMESPACE}" -l grouping=garage-cloud-native-toolkit -l app.kubernetes.io/component=tools -o json | jq '.items[].data | to_entries | select(.[].key | endswith("_URL")) | .[].value' | sed "s/\"//g")
-
-echo "Validating config urls:"
-echo "${CONFIG_URLS}"
-
-echo "${CONFIG_URLS}" | while read url; do
-  if [[ -n "${url}" ]]; then
-    ${SCRIPT_DIR}/waitForEndpoint.sh "${url}" 10 10
-  fi
-done
+if [[ $(kubectl get tasks -n "${NAMESPACE}" | wc -l) -eq 0 ]]; then
+  echo "No pipelines found"
+  exit 1
+fi
 
 exit 0
