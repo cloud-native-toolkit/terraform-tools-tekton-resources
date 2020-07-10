b8ae65498a8ee43a9b8144bb38ca8eccb2530238
Sean Sundberg <seansund@us.ibm.com>
2020-07-10 12:43:54 -0500

    Updates get-latest-resource.sh
    - Extracts the basedir from the output file and creates the directory, if it doesn't exist already
    
    ibm-garage-cloud/planning#453

--- scripts/get-latest-release.sh
+++ scripts/get-latest-release.sh
@@ -4,6 +4,27 @@ GIT_URL="$1"
 RELEASE="$2"
 OUT_FILE="$3"
 
+if [[ -z "${GIT_URL}" ]]; then
+  echo "The GIT_URL is required as the first argument"
+  exit 1
+fi
+
+if [[ -z "${RELEASE}" ]]; then
+  echo "The RELEASE is required as the second argument"
+  exit 1
+fi
+
+if [[ -z "${OUT_FILE}" ]]; then
+  echo "The OUT_FILE is required as the third argument"
+  exit 1
+fi
+
+set -e
+
+BASE_DIR=$(dirname "${OUT_FILE}")
+echo "Creating directory for outfile - ${BASE_DIR}"
+mkdir -p "${BASE_DIR}"
+
 if [[ "${RELEASE}" == "latest" ]]; then
   LATEST_RELEASE=$(curl -s "${GIT_URL}/releases/latest" | grep tag_name | sed -E "s/.*\"tag_name\": \"(.*)\".*/\1/")
   echo "Latest release: ${LATEST_RELEASE}"
