35275f8da00bf28ae7c391940328220832a8a668
Carlos Santana <csantana23@gmail.com>
2020-07-26 15:09:03 -0400

    No need to know the tekton namespace

--- variables.tf
+++ variables.tf
@@ -14,11 +14,6 @@ variable "resource_namespace" {
   default     = "tools"
 }
 
-variable "tekton_namespace" {
-  type        = string
-  description = "The namespace where the tekton service was deployed"
-}
-
 variable "pre_tekton" {
   type        = string
   description = "Flag indicating that the Tekton installed version is pre 0.7.0"
