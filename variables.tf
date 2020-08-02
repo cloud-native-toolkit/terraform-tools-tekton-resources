e95b1a0d8ce0a3b352e8c5164ea045378d13b03b
Sean Sundberg <seansund@us.ibm.com>
2020-08-02 18:19:25 -0500

    Pins revision of tekton tasks to v1.27.0 (#6)
    * Pins revision of tekton tasks to v1.27.0
    * Updates cli-tools image to 0.6.0-lite

--- variables.tf
+++ variables.tf
@@ -28,7 +28,7 @@ variable "pre_tekton" {
 variable "revision" {
   type        = string
   description = "The revision Cloud Native Toolkit Tekton tasks and pipelines"
-  default     = "latest"
+  default     = "v1.27.0"
 }
 
 variable "git_url" {
