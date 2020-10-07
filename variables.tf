edefd65779970bf8f442509242812834473fadcd
Sean Sundberg <seansund@us.ibm.com>
2020-10-07 12:09:49 -0500

    Adds workflows to create PR and automerge PR (#9)

--- variables.tf
+++ variables.tf
@@ -28,7 +28,7 @@ variable "pre_tekton" {
 variable "revision" {
   type        = string
   description = "The revision Cloud Native Toolkit Tekton tasks and pipelines"
-  default     = "v2.0.1"
+  default     = "v2.1.3"
 }
 
 variable "git_url" {
