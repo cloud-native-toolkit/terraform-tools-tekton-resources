767f6e1b5151f8441922b544e73fa7ae7f5af91e
seansund <seansund@users.noreply.github.com>
2020-11-11 16:51:21 +0000

    [create-pull-request] automated change

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ variable "pre_tekton" {
 variable "revision" {
   type        = string
   description = "The revision Cloud Native Toolkit Tekton tasks and pipelines"
-  default     = "v2.1.19"
+  default     = "v2.1.24"
 }
 
 variable "git_url" {
