17d7c0880adc9c445b42ec8771e30708402892dd
Sean Sundberg <seansund@us.ibm.com>
2020-11-17 19:33:03 -0600

    [create-pull-request] automated change (#25)

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ variable "pre_tekton" {
 variable "revision" {
   type        = string
   description = "The revision Cloud Native Toolkit Tekton tasks and pipelines"
-  default     = "v2.1.24"
+  default     = "v2.1.25"
 }
 
 variable "git_url" {
