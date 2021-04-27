449d83f433adfec717b7a8ad0d67173709c59c76
seansund <seansund@users.noreply.github.com>
2021-04-27 21:29:25 +0000

    Updates IBM/ibm-garage-tekton-tasks to v2.6.10

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ variable "pre_tekton" {
 variable "revision" {
   type        = string
   description = "The revision Cloud Native Toolkit Tekton tasks and pipelines"
-  default     = "v2.6.2"
+  default     = "v2.6.10"
 }
 
 variable "git_url" {
