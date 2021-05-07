c1f314fdf65919b72c92603948b3438ae9947d39
Carlos Santana <csantana@us.ibm.com>
2021-05-06 22:52:13 -0400

    pin terraform module for tests (#49)

--- test/stages/stage1-cluster.tf
+++ test/stages/stage1-cluster.tf
@@ -1,5 +1,5 @@
 module "dev_cluster" {
-  source = "github.com/cloud-native-toolkit/terraform-ibm-ocp-vpc.git"
+  source = "github.com/cloud-native-toolkit/terraform-ibm-ocp-vpc.git?ref=v1.2.5"
 
   resource_group_name     = var.resource_group_name
   region                  = var.region
